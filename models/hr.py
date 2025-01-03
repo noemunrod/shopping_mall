628f7639c50f5beb994cac727fdeccc9cb1f607d
noemunrod <noemunrod@alu.edu.gva.es>
2025-01-03 19:35:49 +0100

    Models created without relations

--- models/hr.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import models, fields
-
-class Employee(models.Model):
-    _inherit = 'hr.employee'
-
-    is_teacher=fields.Boolean(string="Is teacher")
--- models/models.py
+++ models/models.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+"""Odoo module for Shopping Mall activities"""
 from odoo import _, api, fields, models  # type: ignore
 
 
-class price(models.Model):
+class Price(models.Model):
+    """Price entity with status and timestamps of activation"""
     _name = 'price.name'
     _description = 'Collection of prices, activated and not'
     id = fields.Integer('price_id')
@@ -11,21 +13,24 @@ class price(models.Model):
     active = fields.Boolean('price_active')
 
 
-class stock(models.Model):
+class Stock(models.Model):
+    """Stock entity with the sum of all lots of his referenced product"""
     _name = 'stock.name'
     _description = 'Stock amount of sum of all lots belonging referenced product'
     sum_of_lots = fields.Integer('sum_of_lots')
 
 
-class lot(models.Model):
+class Lot(models.Model):
+    """Lot entity with lot number, amount of his referenced product and expiration date"""
     _name = 'lot'
     _description = 'Lot individual stock for caducity verification'
-    id = fields.Integer('id')
+    number = fields.Integer('id')
     expiration = fields.Date('expiration')
     amount = fields.Integer('stock')
 
 
-class product(models.Model):
+class Product(models.Model):
+    """Product entity with internal id, name, description, and price and stock references"""
     _name = 'product'
     _description = 'Product base entity'
     id = fields.Integer('id')
@@ -33,13 +38,15 @@ class product(models.Model):
     description = fields.Char('description')
 
 
-class cart_products(models.Model):
+class CartProducts(models.Model):
+    """CartProduct Link for each product and amount of these on a cart"""
     _name = 'cart_products'
     _description = 'cart_products'
     quantity = fields.Integer('quantity')
 
 
-class cart(models.Model):
+class Cart(models.Model):
+    """Cart entity with each cartProduct and payment relevance information"""
     _name = 'cart'
     _description = 'cart'
     amount = fields.Float('amount')
@@ -49,6 +56,15 @@ class cart(models.Model):
     creation_timestamp = fields.Datetime('creation_timestamp')
 
 
-class customer(models.Model):
+class Customer(models.Model):
+    """Customer entity with general data"""
     _name = 'customer'
     _description = 'customer'
+    id = fields.Integer('id')
+    external_uid = fields.Char('external_uid')
+    name = fields.Char('name')
+    surname = fields.Char('surname')
+    email = fields.Char('email')
+    dir_line_1 = fields.Char('dir_line_1')
+    dir_line_2 = fields.Char('dir_line_2')
+    post_code = fields.Integer('post_code')
